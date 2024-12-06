1f6082f0407bc06832d9f839be5d11ba3cc70777
msyilmaz <mselman.yilmaz@spk.gov.tr>
2024-12-06 12:41:48 +0300

    Kullanıcı tarafına Bootstrap altyapısı kuruldu.

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-products',
+  selector: 'app-productss',
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
